db447aa32b3e1a0b979d0d9c42401d0c9777d0b8
Romuald Brillout <git@brillout.com>
2022-05-27 23:14:18 +0200

    fix ts

--- src/config/getConfig.ts
+++ src/config/getConfig.ts
@@ -3,6 +3,8 @@ import { assertUsage } from '../utils'
 import { Config } from './Config'
 
 function getConfig(): Config {
+  // We use `@ts-ignore` because the VikePress user will most likely not have `vite-plugin-glob/client` in his `tsconfig.json`.
+  // @ts-ignore
   const globResult = import.meta.importGlob('/**/vikepress.config.*([a-zA-Z0-9])', { eager: true, import: 'default' })
   const files = Object.keys(globResult)
   assertUsage(files.length >= 1, 'No Vikepress config file found `vikepress.config.(js|ts|tsx|...)`')

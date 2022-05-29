da5aa50e816e4c079ea5f91486c4e2803930faba
Romuald Brillout <git@brillout.com>
2022-05-29 18:26:59 +0200

    downgrade vite-plugin-glob

--- src/config/getConfig.ts
+++ src/config/getConfig.ts
@@ -5,13 +5,13 @@ import { Config } from './Config'
 function getConfig(): Config {
   // We use `@ts-ignore` because the VikePress user will most likely not have `vite-plugin-glob/client` in his `tsconfig.json`.
   // @ts-ignore
-  const globResult = import.meta.importGlob('/**/vikepress.config.*([a-zA-Z0-9])', { eager: true, import: 'default' })
+  const globResult = import.meta.importGlob('/**/vikepress.config.*([a-zA-Z0-9])', { eager: true })
   const files = Object.keys(globResult)
   assertUsage(files.length >= 1, 'No Vikepress config file found `vikepress.config.(js|ts|tsx|...)`')
   assertUsage(
     files.length === 1,
     `Found multiple \`vikepress.config.js\` files: ${files.map((f) => `\`${f}\``).join(', ')}. Define only one instead.`
   )
-  const config = Object.values(globResult)[0] as Config
+  const config = (Object.values(globResult)[0] as any).default as Config
   return config
 }

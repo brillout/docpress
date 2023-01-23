870c54a14c3a27b5b25587222efa7a45f4aafafb
Romuald Brillout <git@brillout.com>
2023-01-23 14:49:51 +0100

    fix: remove superfluous #page-content hash

--- src/algolia/DocSearch.ts
+++ src/algolia/DocSearch.ts
@@ -19,19 +19,37 @@ function getDocSearchJS(pageContext: PageContextResolved) {
     : escapeInject`
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@docsearch/js@alpha"></script>
     <script type="text/javascript">
+      const transformData = ${getTransformData()};
       docsearch({
         appId: '${pageContext.meta.algolia.appId}',
         apiKey: '${pageContext.meta.algolia.apiKey}',
         indexName: '${pageContext.meta.algolia.indexName}',
         container: '#docsearch-desktop',
-      })
+        transformData
+      });
       docsearch({
         appId: '${pageContext.meta.algolia.appId}',
         apiKey: '${pageContext.meta.algolia.apiKey}',
         indexName: '${pageContext.meta.algolia.indexName}',
         container: '#docsearch-mobile',
-      })
+        transformData
+      });
     </script>
   `
   return docSearchJS
+}
+
+// Remove superfluous hash '#page-content' from URLs pointing to whole pages
+//  - https://discourse.algolia.com/t/how-to-avoid-hash-in-search-result-url/6486
+//  - https://discourse.algolia.com/t/docsearchs-transformdata-function-cannot-remove-hashes-from-result-urls/8487
+function getTransformData() {
+  return `function(hits) {
+    hits.map(hit => {
+      if (hit.anchor === 'page-content') {
+        hit.url = hit.url.replace('#'+ hit.anchor, '');
+        hit.anchor = null;
+       }
+    });
+    return hits;
+  }`
 }

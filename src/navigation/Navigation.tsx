928f1d818a750a45ee49b1c92f623169968f4c16
Romuald Brillout <git@brillout.com>
2023-04-03 14:40:46 +0200

    minor

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -22,7 +22,7 @@ function Navigation({
     <>
       <div id="navigation-container">
         <NavigationHeader />
-        {isDetachedPage && <DetachedPageNote />}
+        {isDetachedPage && <DetchedNavigation />}
         <NavigationContent pageContext={pageContext} />
         {/* <ScrollOverlay /> */}
         <NavigationFullscreenClose />
@@ -176,37 +176,11 @@ function getHeadingsWithComputedProps(pageContext: {
   })
 }
 
-function ScrollOverlay() {
-  // const width = '1px'
-  // const color = '#aaa'
+function DetchedNavigation() {
   return (
-    <div
-      id="scroll-overlay"
-      style={{
-        pointerEvents: 'none',
-        position: 'absolute',
-        left: '0',
-        width: '100%',
-        /*
-        background: `linear-gradient(to right, ${color} ${width}, transparent ${width}) 0 0,
-    linear-gradient(to right, ${color} ${width}, transparent ${width}) 0 100%,
-    linear-gradient(to left, ${color} ${width}, transparent ${width}) 100% 0,
-    linear-gradient(to left, ${color} ${width}, transparent ${width}) 100% 100%,
-    linear-gradient(to bottom, ${color} ${width}, transparent ${width}) 0 0,
-    linear-gradient(to bottom, ${color} ${width}, transparent ${width}) 100% 0,
-    linear-gradient(to top, ${color} ${width}, transparent ${width}) 0 100%,
-    linear-gradient(to top, ${color} ${width}, transparent ${width}) 100% 100%`,
-        //*/
-        //borderRight: `5px solid ${color}`,
-        borderRight: `3px solid #666`,
-        //border: `1px solid ${color}`,
-        boxSizing: 'border-box',
-        // backgroundColor: 'rgba(0,0,0,0.03)',
-        backgroundRepeat: 'no-repeat',
-
-        backgroundSize: '10px 10px'
-      }}
-    />
+    <>
+      <DetachedPageNote />
+    </>
   )
 }
 
@@ -238,5 +212,39 @@ function DetachedPageNote() {
         &mdash; this page isn't listed in the navigation below.
       </span>
     </div>
+  )
+}
+
+function ScrollOverlay() {
+  // const width = '1px'
+  // const color = '#aaa'
+  return (
+    <div
+      id="scroll-overlay"
+      style={{
+        pointerEvents: 'none',
+        position: 'absolute',
+        left: '0',
+        width: '100%',
+        /*
+        background: `linear-gradient(to right, ${color} ${width}, transparent ${width}) 0 0,
+    linear-gradient(to right, ${color} ${width}, transparent ${width}) 0 100%,
+    linear-gradient(to left, ${color} ${width}, transparent ${width}) 100% 0,
+    linear-gradient(to left, ${color} ${width}, transparent ${width}) 100% 100%,
+    linear-gradient(to bottom, ${color} ${width}, transparent ${width}) 0 0,
+    linear-gradient(to bottom, ${color} ${width}, transparent ${width}) 100% 0,
+    linear-gradient(to top, ${color} ${width}, transparent ${width}) 0 100%,
+    linear-gradient(to top, ${color} ${width}, transparent ${width}) 100% 100%`,
+        //*/
+        //borderRight: `5px solid ${color}`,
+        borderRight: `3px solid #666`,
+        //border: `1px solid ${color}`,
+        boxSizing: 'border-box',
+        // backgroundColor: 'rgba(0,0,0,0.03)',
+        backgroundRepeat: 'no-repeat',
+
+        backgroundSize: '10px 10px'
+      }}
+    />
   )
 }

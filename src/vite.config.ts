ccbdf42035e8873c781789f2d89e1b7a60f4f2f9
Romuald Brillout <git@brillout.com>
2023-04-03 17:25:13 +0200

    revert "revert "revert "use pageFilesSrc instead of pageFilesDist"""

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -24,7 +24,11 @@ const config: UserConfig = {
       extensions: [
         {
           npmPackageName: '@brillout/docpress',
-          pageFilesSrc: '/src/renderer/*'
+          pageFilesDist: [
+            '@brillout/docpress/renderer/_default.page.server.js',
+            '@brillout/docpress/renderer/_default.page.client.js'
+          ],
+          assetsDir: '/dist/'
         }
       ],
       includeAssetsImportedByServer: true,

6ba0cac4736a341fd1e0ff12bdf06dbeb35a1b39
Romuald Brillout <git@brillout.com>
2023-06-05 11:48:10 +0200

    feat: <CodeBlockTransformer>

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,25 +1,34 @@
 export { CodeBlock }
+export { CodeBlockTransformer }
 
 import React from 'react'
 import { assert } from '../utils/server'
 
-function CodeBlock({ children, lineBreak }: { children: any; lineBreak?: 'white-space' | 'break-word' }) {
+type LineBreak = 'white-space' | 'break-word'
+
+function CodeBlock({ children, lineBreak }: { children: any; lineBreak: LineBreak }) {
+  assertLineBreak(lineBreak)
+  const className = getClassName(lineBreak)
+  return (
+    <pre className={className}>
+      <code>{children}</code>
+    </pre>
+  )
+}
+
+function CodeBlockTransformer({ children, lineBreak }: { children: any; lineBreak: LineBreak }) {
+  assertLineBreak(lineBreak)
+  const className = getClassName(lineBreak)
+  return <div className={className}>{children}</div>
+}
+
+function getClassName(lineBreak: LineBreak) {
+  return `with-line-break_${lineBreak}` as const
+}
+
+function assertLineBreak(lineBreak: LineBreak) {
   assert(
     lineBreak === 'white-space' || lineBreak === 'break-word',
     '`lineBreak` is currently the only use case for <CodeBlock>'
-  )
-  const style: React.CSSProperties = {}
-  if (lineBreak) {
-    style.whiteSpace = 'break-spaces'
-    style.paddingRight = '16px !important'
-    if (lineBreak === 'break-word') {
-      style.wordWrap = 'break-word'
-      style.wordBreak = 'break-all'
-    }
-  }
-  return (
-    <pre>
-      <code style={style}>{children}</code>
-    </pre>
   )
 }

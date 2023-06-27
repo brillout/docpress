d941d325ffece6cd01a425187661374c587c3903
Romuald Brillout <git@brillout.com>
2023-06-27 10:56:56 +0200

    minor refactor

--- src/components/ReadingRecommendation.tsx
+++ src/components/ReadingRecommendation.tsx
@@ -1,46 +1,49 @@
 import React from 'react'
-import { Info } from './Info'
 import { Link } from './Link'
 
 export { ReadingRecommendation }
 
-function ReadingRecommendation({ tour, links }: { tour?: true; links: string[] }) {
+function ReadingRecommendation({ tour, links }: { tour?: true; links?: string[] }) {
+  links ??= []
+  if (!links.length) tour = true
   const multiple = links.length + (tour ? 1 : 0) > 1
+  const intro = <b>Reading Recommendation{multiple ? '.' : ': '}</b>
   return (
-    <Info>
-      {multiple ? ' ' : ''}
-      <b>Reading Recommendation{multiple ? '.' : ': '}</b>
+    <blockquote>
       {(() => {
         if (!multiple) {
           const link = tour ? <TourLink /> : <Link href={links[0]} />
           return (
-            <>
+            <p>
+              {intro}
               {link}
-              {'.'}
-            </>
+            </p>
           )
         }
         return (
-          <ul
-            style={{
-              marginLeft: 18,
-              marginTop: 11
-            }}
-          >
-            {tour && (
-              <li>
-                <TourLink />
-              </li>
-            )}
-            {links.map((link, i) => (
-              <li key={i}>
-                <Link href={link} />
-              </li>
-            ))}
-          </ul>
+          <>
+            <p> {intro}</p>
+            <ul
+              style={{
+                marginLeft: 18,
+                marginTop: 11
+              }}
+            >
+              {tour && (
+                <li>
+                  <TourLink />
+                </li>
+              )}
+              {links.map((link, i) => (
+                <li key={i}>
+                  <Link href={link} />
+                </li>
+              ))}
+            </ul>
+          </>
         )
       })()}
-    </Info>
+    </blockquote>
   )
 }
 

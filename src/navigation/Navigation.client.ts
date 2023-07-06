b88fc65b0aa55f77f0277d6189dc099be5b3fc23
Romuald Brillout <git@brillout.com>
2023-07-06 11:08:16 +0200

    fix: hide mobile navigation overlay when clicking on nav link

--- src/navigation/Navigation.client.ts
+++ src/navigation/Navigation.client.ts
@@ -3,17 +3,30 @@ import { initNavigationFullscreen } from './navigation-fullscreen/initNavigation
 activateNavigationMask()
 activateMenuToggle()
 initNavigationFullscreen()
+autoHideNavigationOverlayOnLinkClick()
 
 function activateMenuToggle() {
   const menuToggle = document.getElementById('menu-toggle')!
-  menuToggle.onclick = toggleNavigation
+  menuToggle.onclick = navigationOverlayToggle
 }
 
 function activateNavigationMask() {
   const navigationMask = document.getElementById('navigation-mask')!
-  navigationMask.onclick = toggleNavigation
+  navigationMask.onclick = navigationOverlayToggle
 }
 
-function toggleNavigation() {
+function autoHideNavigationOverlayOnLinkClick() {
+  document.addEventListener('click', (ev: any) => {
+    const el = ev.target
+    if (!el || !('classList' in el)) return
+    if (!el.classList.contains('nav-item')) return
+    navigationOverlayHide()
+  })
+}
+
+function navigationOverlayToggle() {
   document.body.classList.toggle('show-menu')
+}
+function navigationOverlayHide() {
+  document.body.classList.remove('show-menu')
 }

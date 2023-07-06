277dadba79d1c0ada78e497e776ab388b59a9a40
Romuald Brillout <git@brillout.com>
2023-07-06 12:12:32 +0200

    fix: remove dupliate sponsor

--- src/components/Sponsors/sponsorsList.ts
+++ src/components/Sponsors/sponsorsList.ts
@@ -9,7 +9,6 @@ import inlangLogo from './companyLogos/inlang.png'
 import alignableLogo from './companyLogos/alignable.svg'
 
 const individuals: Sponsor[] = [
-  { username: 'royalswe' },
   { username: 'agalbenus' },
   { username: 'phiberber' },
   { username: 'cookieplace' },

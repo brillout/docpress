ae259137032998b3676af13958f3fce4951aee38
Romuald Brillout <git@brillout.com>
2023-10-27 18:16:08 +0200

    fix: add sponsor

--- src/components/Sponsors/sponsorsList.ts
+++ src/components/Sponsors/sponsorsList.ts
@@ -10,6 +10,7 @@ import bluefinLogo from './companyLogos/bluefin.svg'
 import alignableLogo from './companyLogos/alignable.svg'
 
 const individuals: Sponsor[] = [
+  { username: 'RoyMcCrain' },
   { username: 'BurdaForward' },
   { username: 'chrisvander' },
   { username: 'EralChen' },

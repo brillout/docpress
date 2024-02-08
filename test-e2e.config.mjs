2bbd39ad3a27312cb280b7df1f8156a6ed655c51
Romuald Brillout <git@brillout.com>
2024-02-08 19:47:09 +0100

    tolerate warning

--- test-e2e.config.mjs
+++ test-e2e.config.mjs
@@ -5,6 +5,8 @@ export default {
 function tolerateError({ logText }) {
   return [
     "You are using Vike's deprecated design. Update to the new V1 design, see https://vike.dev/migration/v1-design for how to migrate.",
-    'has been externalized for browser compatibility'
+    'has been externalized for browser compatibility',
+    // TODO remove once Vike `main` is released
+    'The glob option "as" has been deprecated in favour of "query"'
   ].some((txt) => logText.includes(txt))
 }

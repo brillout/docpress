9e731d660f1303b2ca5ca8f1f9d3828e8eaaeb70
Romuald Brillout <git@brillout.com>
2024-03-20 15:46:36 +0100

    prettier

--- src/+config.ts
+++ src/+config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'vike/types'
 
 export default {
   // @ts-ignore Remove this ts-ignore once Vike's new version is released.
-  name: '@brillout/docpress' ,
+  name: '@brillout/docpress',
   onRenderHtml: 'import:@brillout/docpress/renderer/onRenderHtml:onRenderHtml',
   client: 'import:@brillout/docpress/renderer/client:doesNotExist',
   meta: {

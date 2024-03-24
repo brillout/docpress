370b9996c77d8f1ff113482fe742455fff5fa4e1
Romuald Brillout <git@brillout.com>
2024-03-24 23:34:23 +0100

    minor CI fix

--- test-e2e.config.mjs
+++ test-e2e.config.mjs
@@ -6,5 +6,8 @@ function tolerateError({ logText }) {
   return [
     //
     'has been externalized for browser compatibility',
+    // TOOD: remove once Vike's `main` branch is released.
+    // [vike][Warning] /pages/+config.ts uses extends to inherit from /home/runner/work/docpress/docpress/src/dist/+config.js which is a user-land file: this is experimental and may be remove at any time. Reach out to a maintainer if you need this.
+    'which is a user-land file: this is experimental and may be remove at any time. Reach out to a maintainer if you need this.',
   ].some((txt) => logText.includes(txt))
 }

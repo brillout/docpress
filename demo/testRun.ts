91263dce5c0b1307a2e5ae7612e645e95d47d963
Romuald Brillout <git@brillout.com>
2024-03-26 15:28:31 +0100

    minor refactor

--- demo/testRun.ts
+++ demo/testRun.ts
@@ -10,26 +10,29 @@ function testRun(cmd: 'pnpm run dev' | 'pnpm run preview') {
     run(cmd, { additionalTimeout })
   }
 
-  test('/', async () => {
-    const html = await fetchHtml('/')
+  const landingPageUrl = '/'
+  test(landingPageUrl, async () => {
+    const html = await fetchHtml(landingPageUrl)
     expect(html).toContain('Introduction')
     expect(html).toContain('Feature 1')
     expect(html).toContain('Some global note.')
-    await page.goto(getServerUrl() + '/')
+    await page.goto(getServerUrl() + landingPageUrl)
     const text = await page.textContent('body')
     expect(text).toContain('Some global note.')
   })
 
-  test('/orphan', async () => {
-    await page.goto(getServerUrl() + '/orphan')
+  const orphanURL = '/orphan'
+  test(orphanURL, async () => {
+    await page.goto(getServerUrl() + orphanURL)
     const text = await page.textContent('body')
     expect(text).toContain(`This page is "detached": it isn't included in the left-side navigation.`)
     expect(text).toContain(`Another Orphan Page Without Headings.`)
     expect(text).toContain(`Same page link for orphan page: Some SecTion.`)
   })
 
-  test('/features', async () => {
-    await page.goto(getServerUrl() + '/features')
+  const featuresURL = '/features'
+  test(featuresURL, async () => {
+    await page.goto(getServerUrl() + featuresURL)
     const text = await page.textContent('body')
     expect(text).toContain('Some global note.')
     expect(text).toContain('Another Section > Some Page (basic link)')

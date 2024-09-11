6ae4346b6f4de48dfaebdf3063688891267276ff
Romuald Brillout <git@brillout.com>
2024-09-11 13:17:29 +0200

    fix: improve .mdx type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 declare module '*.mdx' {
-  const value: () => JSX.Element
+  const value: (props?: any) => JSX.Element
   export default value
   export const headings: { level: number; title: string; id: string }[]
 }

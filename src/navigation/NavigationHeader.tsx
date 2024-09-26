d3641d3590fc5942d815f0a4aedfc89e3d699cbd
Muhammad <phonzammizaki@gmail.com>
2024-09-27 01:03:42 +0700

    chore: remove unused DocSearch `transformItems`

--- src/navigation/NavigationHeader.tsx
+++ src/navigation/NavigationHeader.tsx
@@ -78,7 +78,6 @@ function Links() {
             appId={algolia.appId}
             indexName={algolia.indexName}
             apiKey={algolia.apiKey}
-            transformItems={transformItems}
             insights={true}
           />
         </div>
@@ -125,18 +124,4 @@ function LinkIcon({ className, icon, href, style }: { className: string; icon: s
       </a>
     </>
   )
-}
-
-// Remove superfluous hash '#page-content' from URLs pointing to whole pages
-//  - https://github.com/algolia/docsearch/issues/1801
-//  - https://discourse.algolia.com/t/how-to-avoid-hash-in-search-result-url/6486
-//  - https://discourse.algolia.com/t/docsearchs-transformdata-function-cannot-remove-hashes-from-result-urls/8487
-type TransformItems = Parameters<typeof DocSearch>[0]['transformItems']
-const transformItems: TransformItems = (hits) => {
-  hits.map((hit) => {
-    if (hit.url.indexOf('#page-content') > 0) {
-      hit.url = hit.url.replace('#page-content', '')
-    }
-  })
-  return hits
 }

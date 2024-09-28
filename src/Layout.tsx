d2293e627d88fde4d128c4c1227b8fcdc4a3af78
Romuald Brillout <git@brillout.com>
2024-09-28 13:00:33 +0200

    refactor: move React wrappers upwards

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,47 +2,36 @@ export { Layout }
 
 import React from 'react'
 import { Navigation, NavigationMask } from './navigation/Navigation'
-import type { PageContextResolved } from './config/resolvePageContext'
 import { MobileHeader } from './MobileHeader'
 import { EditPageNote } from './components/EditPageNote'
-import { PageContextProvider, PageContextProvider2 } from './renderer/usePageContext'
 import './Layout.css'
 import { NavigationFullscreenButton } from './navigation/navigation-fullscreen/NavigationFullscreenButton'
-import type { PageContext } from 'vike/types'
 import { parseTitle } from './parseTitle'
+import { usePageContext } from './renderer/usePageContext'
 
-function Layout({
-  pageContext,
-  children,
-  pageContext2,
-}: { pageContext: PageContextResolved; children: React.ReactNode; pageContext2: PageContext }) {
+function Layout({ children }: { children: React.ReactNode }) {
+  const pageContext = usePageContext()
   const { isLandingPage, pageTitle, navigationData } = pageContext
   const pageTitleParsed = pageTitle && parseTitle(pageTitle)
   const { globalNote } = pageContext.config
   return (
-    <React.StrictMode>
-      <PageContextProvider2 pageContext={pageContext2}>
-        <PageContextProvider pageContext={pageContext}>
-          <div className={`page-layout ${isLandingPage ? 'landing-page' : 'doc-page'}`}>
-            <div id="navigation-wrapper">
-              <Navigation {...pageContext.navigationData} />
-            </div>
-            <NavigationFullscreenButton />
-            <div className="page-wrapper">
-              <div className="page-container">
-                <MobileHeader />
-                <div className="page-content">
-                  {globalNote}
-                  {pageTitleParsed && <h1 id={`${navigationData.currentUrl.replace('/', '')}`}>{pageTitleParsed}</h1>}
-                  {children}
-                  {!isLandingPage && <EditPageNote pageContext={pageContext} />}
-                </div>
-              </div>
-              <NavigationMask />
-            </div>
+    <div className={`page-layout ${isLandingPage ? 'landing-page' : 'doc-page'}`}>
+      <div id="navigation-wrapper">
+        <Navigation {...pageContext.navigationData} />
+      </div>
+      <NavigationFullscreenButton />
+      <div className="page-wrapper">
+        <div className="page-container">
+          <MobileHeader />
+          <div className="page-content">
+            {globalNote}
+            {pageTitleParsed && <h1 id={`${navigationData.currentUrl.replace('/', '')}`}>{pageTitleParsed}</h1>}
+            {children}
+            {!isLandingPage && <EditPageNote pageContext={pageContext} />}
           </div>
-        </PageContextProvider>
-      </PageContextProvider2>
-    </React.StrictMode>
+        </div>
+        <NavigationMask />
+      </div>
+    </div>
   )
 }
--- src/renderer/getPageElement.tsx
+++ src/renderer/getPageElement.tsx
@@ -2,15 +2,22 @@ export { getPageElement }
 
 import type { PageContext } from 'vike/types'
 import type { PageContextResolved } from '../config/resolvePageContext'
+import { PageContextProvider, PageContextProvider2 } from './usePageContext'
 import React from 'react'
 
 function getPageElement(pageContext: PageContext, pageContextResolved: PageContextResolved) {
   const { Page } = pageContext
   const { Layout = PassThrough } = pageContext.config
   const page = (
-    <Layout pageContext={pageContextResolved} pageContext2={pageContext}>
-      <Page />
-    </Layout>
+    <React.StrictMode>
+      <PageContextProvider2 pageContext={pageContext}>
+        <PageContextProvider pageContext={pageContextResolved}>
+          <Layout pageContext={pageContextResolved} pageContext2={pageContext}>
+            <Page />
+          </Layout>
+        </PageContextProvider>
+      </PageContextProvider2>
+    </React.StrictMode>
   )
   return page
 }

9afe6a7a5b0398451525fc008014de92f6b9f025
Romuald Brillout <git@brillout.com>
2024-10-09 21:01:26 +0200

    remove ugly DOM manipulation

--- src/navigation/navigation-fullscreen/initNavigationFullscreen.ts
+++ src/navigation/navigation-fullscreen/initNavigationFullscreen.ts
@@ -20,13 +20,11 @@ function initKeyBindings() {
     },
     false,
   )
-  window.addEventListener('resize', updateColumnWidth, { passive: true })
   initTopNavigation()
 }
 function initNavigationFullscreen() {
   document.getElementById('navigation-fullscreen-button')!.onclick = toggleNavExpend
   document.getElementById('navigation-fullscreen-close')!.onclick = toggleNavExpend
-  updateColumnWidth()
 }
 
 function toggleNavExpend() {
@@ -40,55 +38,6 @@ function toggleNavExpend() {
 function hideNavigationFullScreen() {
   if (!document.documentElement.classList.contains('navigation-fullscreen')) return
   toggleNavExpend()
-}
-
-function updateColumnWidth() {
-  const navMinWidth = 299
-  const navH1Groups = Array.from(document.querySelectorAll('#navigation-content-main .nav-items-group'))
-  const numberOfColumnsMax = navH1Groups.length
-
-  // `window.innerWidth` inlcudes scrollbar width: https://developer.mozilla.org/en-US/docs/Web/API/Window/innerWidth#usage_notes
-  const widthAvailable = document.documentElement.clientWidth
-  const numberOfColumns = Math.max(1, Math.min(numberOfColumnsMax, Math.floor(widthAvailable / navMinWidth)))
-
-  let columns = navH1Groups.map((navH1Group) => {
-    const column = [
-      {
-        element: navH1Group,
-        elementHeight: navH1Group.children.length,
-      },
-    ]
-    return column
-  })
-
-  mergeColumns(columns, numberOfColumns)
-
-  const navContent = document.getElementById('navigation-content-main')!
-
-  Array.from(navContent.children).forEach((child) => {
-    assert(child.className === 'nav-column')
-  })
-  navContent.innerHTML = ''
-
-  columns.forEach((column) => {
-    const columnEl = document.createElement('div')
-    columnEl.className = 'nav-column'
-    column.forEach(({ element }) => {
-      columnEl.appendChild(element)
-    })
-    navContent.appendChild(columnEl)
-  })
-
-  const navItemMaxWidth = 350
-  navContent.style.maxWidth = `${numberOfColumns * navItemMaxWidth}px`
-}
-
-function mergeColumns<T>(columns: { element: T; elementHeight: number }[][], maxNumberOfColumns: number) {
-  assert(columns.length > 0)
-  assert(maxNumberOfColumns > 0)
-  if (columns.length <= maxNumberOfColumns) {
-    return columns
-  }
 }
 
 function determineColumns(columnsUnmerged: number[], maxNumberOfColumns: number): number[] {

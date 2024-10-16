1d44570d7d11ed28949fe46c346cca3c12d2fbe4
Romuald Brillout <git@brillout.com>
2024-10-16 13:16:35 +0200

    improve keybindings

--- src/navigation/navigation-fullscreen/NavigationFullscreenButton.tsx
+++ src/navigation/navigation-fullscreen/NavigationFullscreenButton.tsx
@@ -4,7 +4,7 @@ export { NavigationFullscreenClose }
 
 import React from 'react'
 import closeIcon from './close.svg'
-import { hotkeyLabel } from './hotkeyLabel'
+import { hotkeyMenuClose } from './hotkeyMenu'
 import { toggleMenu } from './initNavigationFullscreen'
 
 function NavigationFullscreenClose() {
@@ -12,7 +12,7 @@ function NavigationFullscreenClose() {
     <a
       onClick={toggleMenu}
       style={{ position: 'absolute', top: 11, right: 11, zIndex: 10 }}
-      aria-label={hotkeyLabel}
+      aria-label={hotkeyMenuClose}
       data-balloon-pos="left"
       data-balloon-blunt
       data-balloon-nofocus

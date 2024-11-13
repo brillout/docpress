f13f8e4f3ab25718322e13cf36948c0881a4872c
Romuald Brillout <git@brillout.com>
2024-11-13 12:59:31 +0100

    fix: remove vike-contributors

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,4 +1,4 @@
 import config from '@brillout/docpress/vite-config'
 // Needed because when @brillout/docpress is linked then Vite treat it as user-land
-config.optimizeDeps!.include!.push(...['@docsearch/react', 'vike-contributors'])
+config.optimizeDeps!.include!.push(...['@docsearch/react'])
 export default config

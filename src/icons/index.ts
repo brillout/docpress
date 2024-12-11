6d747d468e29ae91b4f78afd90eec8d44fff4e8b
Romuald Brillout <git@brillout.com>
2024-12-11 16:17:46 +0100

    feat: add icons

---
+++ src/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as iconGear } from './gear.svg'
+export { default as iconBooks } from './books.svg'
+export { default as iconSeedling } from './seedling.svg'
+export { default as iconMagnifyingGlass } from './magnifying-glass.svg'
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export {
 } from './components'
 export { usePageContext } from './renderer/usePageContext'
 export * from './components/Note'
+export * from './icons/index'
 export { assert } from './utils/assert'
 export type { Config } from './types/Config'
 export type { HeadingDefinition, HeadingDetachedDefinition } from './types/Heading'

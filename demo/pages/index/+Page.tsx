20af5108dd32da22d9ebfab98f2a33a7397f8019
Romuald Brillout <git@brillout.com>
2025-01-03 22:25:09 +0100

    minor

--- demo/pages/index/+Page.tsx
+++ demo/pages/index/+Page.tsx
@@ -1,14 +1,15 @@
 export { Page }
 
 import React from 'react'
+import { LoremIpsum } from './LoremIpsum'
 
 function Page() {
   return (
     <>
       <Block noMargin>
         <Header />
-        <Features />
-        <Features />
+        <LoremIpsum />
+        <LoremIpsum />
         <div style={{ height: 30 }} />
       </Block>
     </>
@@ -20,36 +21,6 @@ function Header() {
     <>
       <h1 style={{ textAlign: 'center', fontSize: '3.4em' }}>Next Generation Docs</h1>
     </>
-  )
-}
-
-function Features() {
-  return (
-    <div>
-      <p>
-        Praesent <em>eu augue lacinia</em>, tincidunt purus nec, ultrices ante. Donec dolor felis, ornare vel augue
-        condimentum.
-      </p>
-      <p>
-        Mauris <code>foo</code> quis scelerisque erat.
-      </p>
-      <p>
-        <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</i>
-      </p>
-      <p>
-        <b>Curabitur</b> gravida urna id ligula volutpat dapibus. Integer accumsan dignissim efficitur. Sed mauris
-        tortor, lobortis at suscipit ac, ultricies eu nunc.
-      </p>
-      <p>
-        Sed molestie tempus &mdash; <b>elementum</b>.
-      </p>
-      <p>
-        Nulla eget egestas magna, non luctus magna. Praesent a tellus molestie nisi feugiat commodo quis vel tellus.
-        Praesent scelerisque turpis et diam cursus, non sodales erat tincidunt.
-      </p>
-      <p>Etiam accumsan neque eu vulputate aliquet. Nulla sit amet sollicitudin velit.</p>
-      <p>Pellentesque bibendum semper tortor id venenatis.</p>
-    </div>
   )
 }
 
---
+++ demo/pages/index/LoremIpsum.tsx
@@ -0,0 +1,33 @@
+export { LoremIpsum }
+
+import React from 'react'
+
+function LoremIpsum() {
+  return (
+    <div>
+      <p>
+        Praesent <em>eu augue lacinia</em>, tincidunt purus nec, ultrices ante. Donec dolor felis, ornare vel augue
+        condimentum.
+      </p>
+      <p>
+        Mauris <code>foo</code> quis scelerisque erat.
+      </p>
+      <p>
+        <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</i>
+      </p>
+      <p>
+        <b>Curabitur</b> gravida urna id ligula volutpat dapibus. Integer accumsan dignissim efficitur. Sed mauris
+        tortor, lobortis at suscipit ac, ultricies eu nunc.
+      </p>
+      <p>
+        Sed molestie tempus &mdash; <b>elementum</b>.
+      </p>
+      <p>
+        Nulla eget egestas magna, non luctus magna. Praesent a tellus molestie nisi feugiat commodo quis vel tellus.
+        Praesent scelerisque turpis et diam cursus, non sodales erat tincidunt.
+      </p>
+      <p>Etiam accumsan neque eu vulputate aliquet. Nulla sit amet sollicitudin velit.</p>
+      <p>Pellentesque bibendum semper tortor id venenatis.</p>
+    </div>
+  )
+}

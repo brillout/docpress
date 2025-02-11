18d01f536e5ab2f717ee867d31f81d5942f09b6f
Romuald Brillout <git@brillout.com>
2025-02-11 12:49:12 +0100

    whitelist warning

--- test-e2e.config.mjs
+++ test-e2e.config.mjs
@@ -4,6 +4,7 @@ export default {
 
 function tolerateError({ logText }) {
   return [
+    'Define Vike settings in +config.js instead of vite.config.js', // TODO/now: remove
     // [12:33:38.464][/demo/test-dev.test.ts][pnpm run dev][stderr] Cannot optimize dependency: @brillout/docpress/renderer/onRenderClient, present in 'optimizeDeps.include'
     'Cannot optimize dependency: @brillout/docpress/renderer/onRenderClient',
     // [10:18:25.341][/demo/test-dev.test.ts][pnpm run dev][stderr] Cannot optimize dependency: @brillout/docpress/Layout, present in 'optimizeDeps.include'

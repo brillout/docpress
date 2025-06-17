0814f2c58b12254340cd01ffb790751c78c34d55
Romuald Brillout <git@brillout.com>
2025-06-17 15:20:00 +0200

    Revert "small refactor"
    This reverts commit ac3fed0688ae40987e6e0789e147018681be5b55.

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,5 @@
 export { Layout }
+export { MenuToggle }
 export { containerQueryMobileLayout }
 export { containerQueryMobileNav as containerQueryMobileMenu }
 export { navLeftWidthMin }
@@ -11,6 +12,7 @@ import { getNavItemsWithComputed, NavItem, NavItemComponent } from './NavItemCom
 import { parseMarkdownMini } from './parseMarkdownMini'
 import { usePageContext2 } from './renderer/usePageContext'
 import { ExternalLinks } from './ExternalLinks'
+import { coseMenuModalOnMouseLeave, openMenuModal, toggleMenuModal } from './MenuModal/toggleMenuModal'
 import { MenuModal } from './MenuModal'
 import { autoScrollNav_SSR } from './autoScrollNav'
 import { SearchLink } from './docsearch/SearchLink'
@@ -21,7 +23,6 @@ import { Style } from './utils/Style'
 import { cls } from './utils/cls'
 import { iconBooks } from './icons'
 import { EditLink } from './EditLink'
-import { menuLinkStyle, MenuToggle } from './MenuToggle'
 
 const blockMargin = 3
 const mainViewPadding = 20
@@ -267,6 +268,12 @@ function NavigationContent(props: {
       {navContent}
     </div>
   )
+}
+
+const menuLinkStyle: React.CSSProperties = {
+  height: '100%',
+  padding: '0 var(--padding-side)',
+  justifyContent: 'center',
 }
 
 function NavHead({ isNavLeft }: { isNavLeft?: true }) {
@@ -521,6 +528,7 @@ function isProjectNameShort(projectName: string) {
   return projectName.length <= 4
 }
 
+let onMouseIgnore: ReturnType<typeof setTimeout> | undefined
 type PropsDiv = React.HTMLProps<HTMLDivElement>
 function MenuToggleMain(props: PropsDiv) {
   return (
@@ -544,6 +552,118 @@ function MenuToggleMain(props: PropsDiv) {
 }
 `}</Style>
     </MenuToggle>
+  )
+}
+function MenuToggle({ menuId, ...props }: PropsDiv & { menuId: number }) {
+  return (
+    <div
+      {...props}
+      style={{
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        cursor: 'pointer',
+        userSelect: 'none',
+        ...menuLinkStyle,
+        ...props.style,
+      }}
+      className={[`colorize-on-hover menu-toggle menu-toggle-${menuId}`, props.className].filter(Boolean).join(' ')}
+      onClick={(ev) => {
+        ev.preventDefault()
+        toggleMenuModal(menuId)
+      }}
+      onMouseEnter={() => {
+        if (onMouseIgnore) return
+        openMenuModal(menuId)
+      }}
+      onMouseLeave={() => {
+        if (onMouseIgnore) return
+        coseMenuModalOnMouseLeave(menuId)
+      }}
+      onTouchStart={() => {
+        onMouseIgnore = setTimeout(() => {
+          onMouseIgnore = undefined
+        }, 1000)
+      }}
+    >
+      <Style>{getAnimation()}</Style>
+      {props.children}
+      <CaretIcon
+        style={{
+          width: 11,
+          marginLeft: 10,
+          flexShrink: 0,
+          color: '#888',
+        }}
+      />
+    </div>
+  )
+
+  function getAnimation() {
+    return css`
+.menu-toggle {
+  position: relative;
+  overflow: hidden;
+  z-index: 0;
+  @media (hover: hover) and (pointer: fine) {
+    .link-hover-animation &:hover::before {
+      top: 0;
+    }
+    html.menu-modal-show & {
+      cursor: default !important;
+    }
+  }
+  &::before {
+    position: absolute;
+    content: '';
+    height: 100%;
+    width: 100%;
+    top: var(--nav-head-height);
+    background-color: var(--active-color);
+    transition-property: top !important;
+    transition: top 0.4s ease !important;
+    z-index: -1;
+  }
+  & .caret-icon-left,
+  & .caret-icon-right {
+    transition: transform .4s cubic-bezier(.4,0, .2, 1);
+  }
+  & .caret-icon-left {
+    transform-origin: 25% 50%;
+  }
+  & .caret-icon-right {
+    transform-origin: 75% 50%;
+  }
+}
+    `
+  }
+}
+function CaretIcon({ style }: { style: React.CSSProperties }) {
+  return (
+    // - Inspired by stripe.com
+    // - Alternative caret SVGs: https://gist.github.com/brillout/dbf05e1fb79a34169cc2d0d5eaf58c01
+    // - The rounded caret (`caret.svg`) doesn't look nice when flipped:
+    // -   https://github.com/brillout/docpress/commit/0ff937d8caf5fc439887ef495e2d2a700234dfb1
+    // - https://github.com/brillout/docpress/pull/39
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 9.24 5.858"
+      style={{
+        overflow: 'visible',
+        ...style,
+      }}
+      className="caret-icon"
+    >
+      <g className="caret-icon-left">
+        <path
+          fill="currentColor"
+          d="m4.001 5.24.619.618 1.237-1.237-.618-.619L4 5.241zm-4-4 4 4L5.24 4.001l-4-4L0 1.241z"
+        ></path>
+      </g>
+      <g className="caret-icon-right">
+        <path fill="currentColor" d="m5.239 5.239-.619.618L3.383 4.62l.618-.619L5.24 5.24Zm4-4-4 4L4 4l4-4z"></path>
+      </g>
+    </svg>
   )
 }
 function DocsIcon() {
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export {
   ImportMeta,
   Emoji,
 } from './components'
-export { MenuToggle } from './MenuToggle'
+export { MenuToggle } from './Layout'
 
 // The only place usePageContext() is used at:
 // https://github.com/vikejs/vike/blob/0b1b109f64aafbed23a1c2ac2630e6146a270ec0/packages/vike.dev/components/CommunityNote.tsx#L4

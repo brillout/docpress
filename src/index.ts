0b920af5f1154ea07e6e63c41a2079f0365ddbe1
Romuald Brillout <git@brillout.com>
2025-06-17 11:32:14 +0200

    comment

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ export {
   Emoji,
 } from './components'
 export { MenuToggle } from './Layout'
+// The only place usePageContext() is used at:
+// https://github.com/vikejs/vike/blob/0b1b109f64aafbed23a1c2ac2630e6146a270ec0/packages/vike.dev/components/CommunityNote.tsx#L4
 export { usePageContext, usePageContext2 } from './renderer/usePageContext'
 export * from './components/Note'
 export * from './icons/index'

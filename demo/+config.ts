49473ab53a9ee03d344882f25865f2b0657ff051
Romuald Brillout <git@brillout.com>
2025-06-20 09:12:01 +0200

    minor refactor

--- demo/+config.ts
+++ demo/+config.ts
@@ -3,7 +3,6 @@ export { config }
 import type { Config } from 'vike/types'
 import docpress from '@brillout/docpress/config'
 
-// Default configs (can be overridden by pages)
 const config = {
   extends: docpress,
 } satisfies Config

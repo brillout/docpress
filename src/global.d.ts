34cfcbac5d5f388b65c1d1a424eb3f1edd572282
Romuald Brillout <git@brillout.com>
2025-07-08 14:03:34 +0200

    fix: fix global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/questions/52005083/how-to-define-css-variables-in-style-attribute-in-react-and-typescript/70398145#70398145
 import 'react'
-declare module 'react' {
+module 'react' {
   interface CSSProperties {
     [key: `--${string}`]: string | number | undefined
   }

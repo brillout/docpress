acb1da050cfad5e0c36c794856b48a83bc1d6c99
Romuald Brillout <git@brillout.com>
2025-08-31 00:26:11 +0200

    fmt

--- src/components/CodeSnippets.tsx
+++ src/components/CodeSnippets.tsx
@@ -65,22 +65,12 @@ function CopyButton() {
   return (
     <button className="copy-button" aria-label={tooltip} data-label-position="top" type="button" onClick={onClick}>
       {!isCopied ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2"
-        >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
           <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
           <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
         </svg>
       ) : (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          stroke="#28a745"
-          strokeWidth="3"
-        >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="#28a745" strokeWidth="3">
           <polyline points="20 6 9 17 4 12" />
         </svg>
       )}

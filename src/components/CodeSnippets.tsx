974bbd39b5dbcd72afe69205136bc78b99076a25
Romuald Brillout <git@brillout.com>
2025-08-31 01:11:12 +0200

    minor refactor

--- src/components/CodeSnippets.tsx
+++ src/components/CodeSnippets.tsx
@@ -59,20 +59,22 @@ function CopyButton() {
     }, 900)
   }
   const tooltip = isSuccess === null ? 'Copy to clipboard' : isSuccess ? 'Copied' : 'Failed'
+  const text =
+    isSuccess === null ? (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+        <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+        <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+      </svg>
+    ) : isSuccess ? (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="#28a745" strokeWidth="3">
+        <polyline points="20 6 9 17 4 12" />
+      </svg>
+    ) : (
+      '❌'
+    )
   return (
     <button className="copy-button" aria-label={tooltip} data-label-position="top" type="button" onClick={onClick}>
-      {isSuccess === null ? (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-          <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-          <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-        </svg>
-      ) : isSuccess ? (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="#28a745" strokeWidth="3">
-          <polyline points="20 6 9 17 4 12" />
-        </svg>
-      ) : (
-        '❌'
-      )}
+      {text}
     </button>
   )
   async function onClick(e: React.MouseEvent<HTMLButtonElement>) {

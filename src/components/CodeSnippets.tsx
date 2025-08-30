fe225e0dfb61bb23cffea3c7c5ddc902aadacbb0
Romuald Brillout <git@brillout.com>
2025-08-31 00:36:31 +0200

    refactor copy button state

--- src/components/CodeSnippets.tsx
+++ src/components/CodeSnippets.tsx
@@ -7,7 +7,7 @@ export { CodeSnippet }
 
 import React, { useState } from 'react'
 import { useSelectCodeLang } from './CodeSnippets/useSelectCodeLang'
-import { assertWarning } from '../utils/assert'
+import { assert, assertWarning } from '../utils/assert'
 import './CodeSnippets.css'
 
 /** Only show if TypeScript is selected */
@@ -52,39 +52,37 @@ function CodeSnippet({
 }
 
 function CopyButton() {
-  const tooltipInit = 'Copy to clipboard'
-  let [tooltip, setTooltip] = useState(tooltipInit)
-  const [isCopied, setIsCopied] = useState(false)
-  const show = (msg: string) => {
-    setTooltip(msg)
+  const [isSuccess, setIsSuccess] = useState(null as null | boolean)
+  const onCopy = (success: boolean) => {
+    setIsSuccess(success)
     setTimeout(() => {
-      setTooltip(tooltipInit)
-      setIsCopied(false)
+      setIsSuccess(null)
     }, 850)
   }
+  const tooltip = isSuccess === null ? 'Copy to clipboard' : isSuccess ? 'Copied' : 'Copy failed'
   return (
     <button className="copy-button" aria-label={tooltip} data-label-position="top" type="button" onClick={onClick}>
-      {!isCopied ? (
+      {isSuccess === null ? (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
           <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
           <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
         </svg>
-      ) : (
+      ) : isSuccess ? (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="#28a745" strokeWidth="3">
           <polyline points="20 6 9 17 4 12" />
         </svg>
+      ) : (
+        '❌'
       )}
     </button>
   )
   function onSuccess() {
-    show('Copied!')
-    setIsCopied(true)
+    onCopy(true)
   }
   function onError(error: unknown) {
     console.error(error)
-    const msg = 'Copy to clipboard failed ❌'
-    show(msg)
-    assertWarning(false, msg)
+    onCopy(false)
+    assertWarning(false, tooltip)
   }
   async function onClick(e: React.MouseEvent<HTMLButtonElement>) {
     try {

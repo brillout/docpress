317b144f66ec8afc85c71c58b44aee1ddfdfcee5
Muhammad <phonzammizaki@gmail.com>
2025-09-10 00:49:50 +0700

    Add `onlyRemoveTypeImports: true` to the internal `@babel/preset-typescript` config preset

--- src/detypePlugin.ts
+++ src/detypePlugin.ts
@@ -78,6 +78,12 @@ async function transformCode(code: string, moduleId: string) {
       // Remove TypeScript
       if (!isYaml) {
         codeBlockContentJs = await detype(codeBlockContentJs, `some-dummy-filename.${codeBlockLang}`, {
+          customizeBabelConfig(config) {
+            // Add `onlyRemoveTypeImports: true` to the internal `@babel/preset-typescript` config
+            // See https://github.com/cyco130/detype/blob/main/src/transform.ts#L206
+            assertUsage(config.presets && config.presets.length === 1, 'Unexpected Babel config presets')
+            config.presets = [[config.presets[0], { onlyRemoveTypeImports: true }]]
+          },
           removeTsComments: true,
           prettierOptions,
         })

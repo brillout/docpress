3a312f0edb623dd6db6c966d9e3505cb0802c35f
Muhammad <phonzammizaki@gmail.com>
2025-09-22 17:36:17 +0700

    update comment

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,10 @@ export { MenuToggle } from './Layout'
 // https://github.com/vikejs/vike/blob/0b1b109f64aafbed23a1c2ac2630e6146a270ec0/packages/vike.dev/components/CommunityNote.tsx#L4
 export { usePageContext } from './renderer/usePageContext'
 
-// We provide our own `useMDXComponents()` to enable MDX component injection by setting
-// `providerImportSource: ` to '@brillout/docpress'.
-// (See https://mdxjs.com/guides/injecting-components/ for details.)
+// We provide our own `useMDXComponents()` to enable MDX component injection by setting `providerImportSource` to '@brillout/docpress'.
+// (See https://mdxjs.com/guides/injecting-components/)
 export { useMDXComponents } from './components/MDXComponents'
+
 export * from './components/Note'
 export * from './icons/index'
 export { assert } from './utils/assert'

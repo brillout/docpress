6f0cf9883be4ef678f98e4c83914eb6bbc2a272c
Romuald Brillout <git@brillout.com>
2025-09-23 13:18:56 +0200

    comment

--- src/components/Pre.tsx
+++ src/components/Pre.tsx
@@ -22,13 +22,15 @@ function CopyButton() {
     }, 900)
   }
   const tooltip = isSuccess === null ? 'Copy to clipboard' : isSuccess ? 'Copied' : 'Failed'
-  const text =
+  const icon =
     isSuccess === null ? (
+      // Copy icon
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
         <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
         <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
       </svg>
     ) : isSuccess ? (
+      // Green checkmark
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="#28a745" strokeWidth="3">
         <polyline points="20 6 9 17 4 12" />
       </svg>
@@ -43,7 +45,7 @@ function CopyButton() {
       type="button"
       onClick={onClick}
     >
-      {text}
+      {icon}
     </button>
   )
   async function onClick(e: React.MouseEvent<HTMLButtonElement>) {

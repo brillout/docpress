a1ebade79ad08a321a4d7acb17431ed19140d9f8
Romuald Brillout <git@brillout.com>
2025-09-23 13:11:22 +0200

    comment

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+/**********/
+/* PUBLIC */
+/**********/
 export {
   CodeBlockTransformer,
   Link,
@@ -15,14 +18,15 @@ export { assert } from './utils/assert'
 export { parseMarkdownMini } from './parseMarkdownMini'
 export type { Config } from './types/Config'
 export type { HeadingDefinition, HeadingDetachedDefinition } from './types/Heading'
-
 // The only place usePageContext() is used at:
 // https://github.com/vikejs/vike/blob/0b1b109f64aafbed23a1c2ac2630e6146a270ec0/packages/vike.dev/components/CommunityNote.tsx#L4
 export { usePageContext } from './renderer/usePageContext'
 
-// Used internally by DocPress — the user (should) never use it
+// The following are used internally by DocPress — users (should) never use these exports
+/************/
+/* INTERNAL */
+/************/
 export { CodeSnippets } from './components'
-
 // We provide our own `useMDXComponents()` to enable MDX component injection by setting `providerImportSource` to '@brillout/docpress'.
 // https://mdxjs.com/guides/injecting-components/
 export { useMDXComponents } from './components/useMDXComponents'

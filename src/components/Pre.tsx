15ca24232b6afebbb24d840bbc7e733e5b3cb742
Muhammad <phonzammizaki@gmail.com>
2025-10-03 17:32:15 +0700

    chore: set copy button SVG stroke to `black`

--- src/components/Pre.tsx
+++ src/components/Pre.tsx
@@ -26,7 +26,7 @@ function CopyButton() {
   const icon =
     isSuccess === null ? (
       // Copy icon
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="black" strokeWidth="2">
         <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
         <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
       </svg>

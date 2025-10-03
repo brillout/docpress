bde6726c4793ebd9741e163185262bef3233ed56
Muhammad <phonzammizaki@gmail.com>
2025-10-03 17:01:09 +0700

    missing a comma

--- src/components/CodeSnippets/useSelectCodeLang.ts
+++ src/components/CodeSnippets/useSelectCodeLang.ts
@@ -54,7 +54,7 @@ function useSelectCodeLang() {
   return [codeLangSelected, selectCodeLang] as const
 }
 
-// WARNING: We cannot use variables (`storageKey` & `codeLangDefaultClient` ) here
+// WARNING: We cannot use variables (`storageKey` & `codeLangDefaultClient` ) here,
 // because their actual values will not appear when calling `initializeJsToggle.toString()`.
 function initializeJsToggle() {
   const codeLangSelected = localStorage.getItem('docpress:code-lang') ?? 'js'

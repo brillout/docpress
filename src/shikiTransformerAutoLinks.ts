fcb98b991ba1b8d907c458433df9ea2846d76e0f
Muhammad <phonzammizaki@gmail.com>
2025-10-05 16:24:10 +0700

    keep comments minimal

--- src/shikiTransformerAutoLinks.ts
+++ src/shikiTransformerAutoLinks.ts
@@ -18,7 +18,6 @@ function shikiTransformerAutoLinks(): ShikiTransformer {
       let child = span.children[0]
       if (child.type !== 'text') return
 
-      // Find all matching URLs in the text node's value using the regex.
       const links: { href: string; index: number }[] = []
       const matches = Array.from(child.value.matchAll(linkRE))
 
@@ -31,14 +30,11 @@ function shikiTransformerAutoLinks(): ShikiTransformer {
         links.unshift({ href, index: match.index })
       }
 
-      // Prepare a new list of children to replace the original span's children.
       const newChildren: typeof span.children = []
-      // Iterate over the matched links.
       for (const { href, index } of links) {
         const postIndex = index + href.length
         const postValue = child.value.slice(postIndex)
 
-        // Preserve any text after the URL as a text node.
         if (postValue.length > 0) {
           newChildren.unshift({ type: 'text', value: postValue })
         }
@@ -51,19 +47,16 @@ function shikiTransformerAutoLinks(): ShikiTransformer {
           children: [{ type: 'text', value: href }],
         })
 
-        // Update remaining text before the URL for the next iteration.
         child = {
           type: 'text',
           value: child.value.slice(0, index),
         }
       }
 
-      // Add any remaining text before the first URL.
       if (child.value.length > 0) {
         newChildren.unshift(child)
       }
 
-      // Replace the original span's children.
       span.children = newChildren
     },
   }

09af308713f26cbda9d950b32740f47e46a83e9b
Muhammad <phonzammizaki@gmail.com>
2025-10-06 23:03:49 +0700

    remove "re-exporting proxy files"

--- src/code-block-controls/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './rehypeMetaToProps.js'
-export { remarkDetype } from './remarkDetype.js'
-export { shikiTransformerAutoLinks } from './shikiTransformerAutoLinks.js'
--- src/vite.config.ts
+++ src/vite.config.ts
@@ -7,7 +7,9 @@ import { parsePageSections } from './parsePageSections.js'
 import rehypePrettyCode from 'rehype-pretty-code'
 import remarkGfm from 'remark-gfm'
 import { transformerNotationDiff } from '@shikijs/transformers'
-import { rehypeMetaToProps, remarkDetype, shikiTransformerAutoLinks } from './code-block-controls/index.js'
+import { rehypeMetaToProps } from './code-block-controls/rehypeMetaToProps.js'
+import { remarkDetype } from './code-block-controls/remarkDetype.js'
+import { shikiTransformerAutoLinks } from './code-block-controls/shikiTransformerAutoLinks.js'
 
 const root = process.cwd()
 const prettyCode = [

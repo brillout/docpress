823bcd312dcb5fe7fa9db14f07727aec37aeb3ec
Romuald Brillout <git@brillout.com>
2025-10-26 18:31:52 +0100

    Revert "wip"
    This reverts commit 6e06e5c1f791ab5763ac2751e6277590e1d1f0fc.

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -392,11 +392,6 @@ function NavHead({ isNavLeft }: { isNavLeft?: true }) {
     padding: 0 var(--padding-side);
   }
 }
-@container container-nav-head (max-width: ${containerQuerySmallNav}px) {
-  .nav-head-content {
-    --padding-side: 0px !important;
-  }
-}
 @container container-viewport (max-width: ${containerQueryMobileNav}px) {
   .hide-on-shrink {
     display: none !important;
@@ -405,7 +400,7 @@ function NavHead({ isNavLeft }: { isNavLeft?: true }) {
 @container container-nav-head (max-width: ${containerQueryMobileNav}px) {
   .nav-head-content {
     --icon-text-padding: min(8px, 7 * (1cqw - 2.5px));
-    --padding-side: 0px;
+    --padding-side: min(24px, 20 * (1cqw - 2.5px));
   }
 }
 @container container-nav-head (min-width: ${containerQueryMobileNav + 1}px) {

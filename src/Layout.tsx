9354ab614efb71a975f69344d6cba7a1af6402e9
Romuald Brillout <git@brillout.com>
2025-10-27 12:11:13 +0100

    minor refactor

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -384,17 +384,16 @@ function NavHead({ isNavLeft }: { isNavLeft?: true }) {
   function getStyleNavHead() {
     let style = ''
 
-    // TODO: don't use important ?
     // Mobile
     style += css`
 @container container-viewport (max-width: ${viewMobile}px) {
   .nav-logo {
-    always-shown: flex-start !important;
+    always-shown: flex-start;
     padding-left: var(--main-view-padding);
   }
   .menu-button {
-    justify-content: flex-end !important;
-    padding-right: var(--main-view-padding) !important;
+    justify-content: flex-end;
+    padding-right: var(--main-view-padding);
   }
   .nav-head-content {
     --icon-text-padding: min(8px, 1.3cqw);

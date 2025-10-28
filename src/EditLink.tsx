81f7000f253e4bd533f8c4bef8054a97fbdfe5c5
Romuald Brillout <git@brillout.com>
2025-10-28 19:38:18 +0100

    polish

--- src/EditLink.tsx
+++ src/EditLink.tsx
@@ -12,18 +12,7 @@ function EditLink({
 }: { className?: string; style: React.CSSProperties; verbose?: boolean }) {
   const pageContext = usePageContext()
   const iconSize = 13
-  const icon = (
-    <img
-      src={iconPencil}
-      width={iconSize}
-      height={iconSize}
-      style={{
-        marginRight: 6,
-        position: 'relative',
-        top: -1,
-      }}
-    />
-  )
+  const icon = <img src={iconPencil} width={iconSize} height={iconSize} style={{ marginRight: 6 }} />
   const { urlPathname } = pageContext
   const fsPath = urlPathname === '/' ? '/index/+Page.tsx' : `${urlPathname}/+Page.mdx`
   const docsDir = pageContext.globalContext.config.docpress.docsDir ?? 'docs'
@@ -35,16 +24,17 @@ function EditLink({
       style={{
         display: 'inline-flex',
         alignItems: 'center',
-        paddingTop: 4,
-        paddingBottom: 1,
+        paddingTop: 7,
+        paddingBottom: 7,
         paddingLeft: 8,
-        paddingRight: 8,
+        paddingRight: 7,
         border: '1px solid #e0e0e0',
         borderRadius: 7,
         fontSize: '0.91em',
         color: '#6c6c6c',
         background: '#f8f8f8',
         letterSpacing: 0.4,
+        lineHeight: 0,
         ...style,
       }}
     >

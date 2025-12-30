0fa9544bccc08579db64dfddc47418a32ae72299
Romuald Brillout <git@brillout.com>
2025-12-31 00:17:52 +0100

    comment

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -230,7 +230,7 @@ function NavLeft() {
                 paddingBottom: 40,
                 minWidth: navLeftWidthMin,
                 width: '100%',
-                // Fade content at top/bottom edges to 30% opacity starting from 20px at the extremities
+                // Fade content at top/bottom scroll edges to 30% opacity starting from 20px at the extremities
                 maskImage:
                   'linear-gradient(to bottom, rgba(0,0,0,0.3) 0px, black 20px, black calc(100% - 20px), rgba(0,0,0,0.3) 100%)',
               }}

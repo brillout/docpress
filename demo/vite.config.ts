eee9db77e73dd9806e79c16171c8d2b3ab9134cf
Romuald Brillout <git@brillout.com>
2025-12-30 16:08:52 +0100

    add vike plugin

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,4 +1,10 @@
+import vike from 'vike/plugin'
+
 export default {
+  plugins: [
+    // Used by the landing page, see `.svg?react` imports
+    vike(),
+  ],
   /* DEBUG
   build: { minify: false },
   //*/

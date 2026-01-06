1d029d871778f84295b13191271844153072e6af
Muhammad <phonzammizaki@gmail.com>
2026-01-06 22:38:17 +0700

    fix: remove line-start anchor (^) from `@detype-replace` regex
    - trim leading whitespace before applying the replacement

--- src/code-blocks/remarkDetype.ts
+++ src/code-blocks/remarkDetype.ts
@@ -164,14 +164,15 @@ function cleanUpCode(code: string, isJsCode: boolean = false) {
 }
 function processMagicComments(code: string) {
   // @detype-replace DummyLayout Layout
-  const renameCommentRE = /^\/\/\s@detype-replace\s([^ ]+) ([^ ]+)\n/gm
+  const renameCommentRE = /\/\/\s@detype-replace\s([^ ]+) ([^ ]+)\n/gm
   const matches = Array.from(code.matchAll(renameCommentRE))
 
   if (matches.length) {
     for (let i = matches.length - 1; i >= 0; i--) {
       const match = matches[i]
       const [fullMatch, renameFrom, renameTo] = match
-      code = code.split(fullMatch).join('').replaceAll(renameFrom, renameTo)
+      const [before, after] = code.split(fullMatch)
+      code = before + after.trimStart().replaceAll(renameFrom, renameTo)
     }
   }
   code = code.replaceAll('// @detype-uncomment ', '')

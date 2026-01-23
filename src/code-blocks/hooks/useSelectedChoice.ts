e0dc97493b5071b9c24d7578b7508a96402f3dce
Muhammad <39896528+phonzammi@users.noreply.github.com>
2026-01-23 20:29:01 +0700

    fix: add `choice` namespace to prevent localStorage key collisions (#94)

--- src/code-blocks/hooks/useSelectedChoice.ts
+++ src/code-blocks/hooks/useSelectedChoice.ts
@@ -17,7 +17,7 @@ const keyPrefix = 'docpress'
 function useSelectedChoice(persistId: string | null, defaultValue: string) {
   if (!persistId) return useState(defaultValue)
 
-  return useLocalStorage(`${keyPrefix}:${persistId}`, defaultValue)
+  return useLocalStorage(`${keyPrefix}:choice:${persistId}`, defaultValue)
 }
 
 // WARNING: We cannot use the keyPrefix variable here: closures don't work because we serialize the function.
@@ -26,7 +26,7 @@ function initializeChoiceGroup() {
   const groupsElements = document.querySelectorAll<HTMLDivElement>('[data-group-name]')
   for (const groupEl of groupsElements) {
     const groupName = groupEl.getAttribute('data-group-name')!
-    const selectedChoice = localStorage.getItem(`docpress:${groupName}`)
+    const selectedChoice = localStorage.getItem(`docpress:choice:${groupName}`)
     if (!selectedChoice) continue
     const selectEl = groupEl.querySelector<HTMLSelectElement>(`.select-choice`)
     if (selectEl) selectEl.value = selectedChoice

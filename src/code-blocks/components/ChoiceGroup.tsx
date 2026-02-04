8a8ffbb482d3a64c9c53330d96b43be86d66d0e7
Muhammad <phonzammizaki@gmail.com>
2026-02-05 01:06:54 +0700

    refactor ChoiceGroup

--- src/code-blocks/components/ChoiceGroup.tsx
+++ src/code-blocks/components/ChoiceGroup.tsx
@@ -1,36 +1,26 @@
 export { ChoiceGroup }
 
 import React from 'react'
-import { usePageContext } from '../../renderer/usePageContext.js'
 import { useSelectedChoice } from '../hooks/useSelectedChoice.js'
 import { useRestoreScroll } from '../hooks/useRestoreScroll.js'
-import { assertUsage } from '../../utils/assert.js'
 import { cls } from '../../utils/cls.js'
-import type { PageContext } from 'vike/types'
 import './ChoiceGroup.css'
 
-const CHOICES_BUILT_IN: Record<string, { choices: string[]; default: string }> = {
-  codeLang: {
-    choices: ['JavaScript', 'TypeScript'],
-    default: 'JavaScript',
-  },
-  pkgManager: {
-    choices: ['npm', 'pnpm', 'Bun', 'Yarn'],
-    default: 'npm',
-  },
+type TChoiceGroup = {
+  name: string
+  choices: string[]
+  default: string
+  disabled: string[]
 }
 
 function ChoiceGroup({
   children,
-  choices,
+  choiceGroup,
   lvl,
   hide = false,
-}: { children: React.ReactNode; choices: string[]; lvl: number; hide: boolean }) {
-  const pageContext = usePageContext()
-  const choiceGroup = findChoiceGroup(pageContext, choices)
-  const [selectedChoice, setSelectedChoice] = useSelectedChoice(choiceGroup.name, choiceGroup.default!)
+}: { children: React.ReactNode; choiceGroup: TChoiceGroup; lvl: number; hide: boolean }) {
+  const [selectedChoice, setSelectedChoice] = useSelectedChoice(choiceGroup.name, choiceGroup.default)
   const prevPositionRef = useRestoreScroll([selectedChoice])
-  const isHidden = choices.length === 1 || !choices.includes(selectedChoice) || hide
 
   return (
     <div data-choice-group={choiceGroup.name} className="choice-group">
@@ -38,11 +28,11 @@ function ChoiceGroup({
         name={`choicesFor-${choiceGroup.name}`}
         value={selectedChoice}
         onChange={onChange}
-        className={cls(['select-choice', isHidden && 'hidden'])}
+        className={cls(['select-choice', hide && 'hidden'])}
         style={{ '--lvl': lvl }}
       >
         {choiceGroup.choices.map((choice, i) => (
-          <option key={i} value={choice} disabled={!choices.includes(choice)}>
+          <option key={i} value={choice} disabled={choiceGroup.disabled.includes(choice)}>
             {choice}
           </option>
         ))}
@@ -56,39 +46,4 @@ function ChoiceGroup({
     prevPositionRef.current = { top: el.getBoundingClientRect().top, el }
     setSelectedChoice(el.value)
   }
-}
-
-function findChoiceGroup(pageContext: PageContext, choices: string[]) {
-  const { choices: choicesConfig } = pageContext.globalContext.config
-  const choicesAll = { ...CHOICES_BUILT_IN, ...choicesConfig }
-
-  const groupName = Object.keys(choicesAll).find((key) => {
-    // get only the values that exist in both choices and choicesAll[key].choices
-    const relevantChoices = choicesAll[key]!.choices.filter((choice) => choices.includes(choice))
-    // if nothing exists, skip this key
-    if (relevantChoices.length === 0) return false
-
-    // check order
-    let i = 0
-    for (const choice of choices) {
-      if (choice === relevantChoices[i]) i++
-    }
-    assertUsage(
-      i === relevantChoices.length,
-      `Choices exist for key "${key}" but NOT in order. Expected order: [${relevantChoices}], got: [${choices}]`,
-    )
-
-    return true
-  })
-  assertUsage(groupName, `Missing group name for [${choices}]. Define it in +docpress.choices.`)
-
-  const mergedChoices = [...new Set([...choices, ...choicesAll[groupName]!.choices])]
-
-  const choiceGroup = {
-    name: groupName,
-    ...choicesAll[groupName]!,
-    choices: mergedChoices,
-  }
-
-  return choiceGroup
 }

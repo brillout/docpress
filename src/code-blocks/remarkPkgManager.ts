ee5480ff183b291591d834baa9811444be3d4aed
Muhammad <phonzammizaki@gmail.com>
2026-02-14 23:03:54 +0700

    use `assertUsage`

--- src/code-blocks/remarkPkgManager.ts
+++ src/code-blocks/remarkPkgManager.ts
@@ -1,21 +1,30 @@
 export { remarkPkgManager }
 
 import type { Code, Root } from 'mdast'
+import type { VFile } from '@mdx-js/mdx/internal-create-format-aware-processors'
 import { visit } from 'unist-util-visit'
 import convert_ from 'npm-to-yarn'
 import { parseMetaString } from './rehypeMetaToProps.js'
 import { generateChoiceGroupCode } from './utils/generateChoiceGroupCode.js'
+import { assertUsage } from '../utils/assert.js'
+import pc from '@brillout/picocolors'
 // @ts-expect-error The type of npm-to-yarn doesn't work with `"moduleResolution": "Node16"`
 const convert: (str: string, to: 'npm' | 'yarn' | 'pnpm' | 'bun') => string = convert_
 
 const PKG_MANAGERS = ['pnpm', 'Bun', 'Yarn'] as const
 
 function remarkPkgManager() {
-  return function (tree: Root) {
+  return function (tree: Root, file: VFile) {
     visit(tree, 'code', (node, index, parent) => {
       if (!parent || typeof index === 'undefined') return
       if (!['bash', 'sh', 'shell'].includes(node.lang || '')) return
       if (node.value.indexOf('npm ') === -1 && node.value.indexOf('npx ') === -1) return
+      assertUsage(
+        !node.value.includes('pnpm'),
+        `Found a 'pnpm' command in the code block at: ${pc.bold(pc.blue(file.path))}, line ${
+          node.position?.start.line
+        }. Replace it with the equivalent 'npm' command to proceed.`,
+      )
       let choice: string | undefined = undefined
       const nodes = new Map<string, Code>()
 
@@ -25,10 +34,6 @@ function remarkPkgManager() {
         node.meta = meta.rest
       }
 
-      if (node.value.includes('pnpm')) {
-        console.warn('Should only use `npm`/`npx` commands')
-        node.value.replaceAll('pnpm', 'npm')
-      }
       node.value = node.value.replaceAll('npm i ', 'npm install ')
       nodes.set('npm', node)
 

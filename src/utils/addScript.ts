42820a52339664079d2f2be997f071b28b645edd
ambergristle <101149854+ambergristle@users.noreply.github.com>
2026-03-15 14:41:02 -0700

    feat: add config to `addScript` to allow attribute setting

--- src/utils/addScript.ts
+++ src/utils/addScript.ts
@@ -2,12 +2,17 @@ export { addScript }
 
 import { genPromise } from './genPromise.js'
 
-async function addScript(src: string) {
+async function addScript(src: string, attributes?: Record<string, string>) {
   const { promise, resolve, reject } = genPromise()
   const script = document.createElement('script')
   script.onload = () => resolve()
   script.onerror = () => reject(new Error(`Failed to load script: ${src}`))
   script.src = src
+  if (attributes) {
+    for (const [name, value] of Object.entries(attributes)) {
+      script.setAttribute(name, value)
+    }
+  }
   document.head.appendChild(script)
   return promise
 }
